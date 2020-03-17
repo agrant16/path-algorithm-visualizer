025c2c196291bfd4f74f526de8a51c0beb0cdf84
Alan Grant <agrant16@utk.edu>
2020-03-17 15:51:57 -0400

    Implemented BFS class.

--- src/algorithms/BFS.js
+++ src/algorithms/BFS.js
@@ -3,5 +3,25 @@ import Pathfinder from "./Pathfinder";
 export default class BFS extends Pathfinder {
   static weighted = false;
 
-  traverse(grid, startNode, endNode) {}
+  traverse(grid, startNode, endNode) {
+    const unvisited = [];
+    const visitedNodesInOrder = [];
+    startNode.isVisted = true;
+    startNode.previous = null;
+    unvisited.push(startNode);
+    visitedNodesInOrder.push(startNode);
+    while (unvisited.length !== 0) {
+      let currentNode = unvisited.shift();
+      if (currentNode === endNode) {
+        return visitedNodesInOrder;
+      }
+      let neighbors = this.getUnvisitedNeighbors(currentNode, grid);
+      for (const neighbor of neighbors) {
+        neighbor.isVisited = true;
+        neighbor.previous = currentNode;
+        unvisited.push(neighbor);
+        visitedNodesInOrder.push(neighbor);
+      }
+    }
+  }
 }
