55b92167a15beb1bdeaf89aad0d3d2e21c536659
Alan Grant <agrant16@utk.edu>
2020-03-17 11:55:49 -0400

    Updates

---
+++ precache-manifest.b267bbec1ba501bde8287bb5b9e6ad53.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e9547af80c00cfb52ad39893c02d331",
+    "url": "/path-algorithm-visualizer/index.html"
+  },
+  {
+    "revision": "9b43dd6c589ebda537d2",
+    "url": "/path-algorithm-visualizer/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "58ec2eb88f5c3b545cda",
+    "url": "/path-algorithm-visualizer/static/css/main.d9a6c1fa.chunk.css"
+  },
+  {
+    "revision": "9b43dd6c589ebda537d2",
+    "url": "/path-algorithm-visualizer/static/js/2.9ba443d6.chunk.js"
+  },
+  {
+    "revision": "95fe7b47190d8ffb46ace11b00922862",
+    "url": "/path-algorithm-visualizer/static/js/2.9ba443d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58ec2eb88f5c3b545cda",
+    "url": "/path-algorithm-visualizer/static/js/main.bd8ef4f7.chunk.js"
+  },
+  {
+    "revision": "8ee94d38b37630ae5eb1",
+    "url": "/path-algorithm-visualizer/static/js/runtime-main.ff4a56e4.js"
+  }
+]);
--- src/constants.js
+++
@@ -1,6 +0,0 @@
-/* Constants used in the app*/
-
-export const START_ROW = 9;
-export const START_COL = 9;
-export const END_ROW = 9;
-export const END_COL = 39;
